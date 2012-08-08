a94601634a51df62d7a972d4cad5470a2db0219e
Scott Albertson <salbertson@ezpublishing.com>
2012-08-07 22:38:15 -0700

    Implement #placeMarker function for board.

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -12,4 +12,30 @@ describe('Board', function() {
       });
     });
   });
+
+  describe('#placeMarker', function() {
+    describe('in one cell', function() {
+      beforeEach(function() {
+        board.placeMarker('x', 0, 0);
+      });
+
+      it('does not cover the board', function() {
+        expect(board.covered()).toBe(false);
+      });
+    });
+
+    describe('in every cell', function() {
+      beforeEach(function() {
+        _.times(3, function(row) {
+          _.times(3, function(column) {
+            board.placeMarker('x', row, column);
+          });
+        });
+      });
+
+      it('covers the board', function() {
+        expect(board.covered()).toBe(true);
+      });
+    });
+  });
 });
--- tictactoe/board.js
+++ tictactoe/board.js
@@ -9,6 +9,10 @@ Board = function() {
 Board.cellCount = 9;
 
 Board.prototype.covered = function() {
-  markers = _.compact(this.data);
+  markers = _.chain(this.data).flatten().compact().value();
   return markers.length == Board.cellCount;
+};
+
+Board.prototype.placeMarker = function(marker, row, column) {
+  this.data[row][column] = marker;
 };
